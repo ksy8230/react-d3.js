e3b656617917a8cb7cc85a1aca378530e8286107
ksy <ksy8230@hanmail.net>
2020-05-17 08:59:54 +0900

    d3 속성값들을 이용해 배열의 값을 radius로 원들 그리기

---
+++ react-d3/src/App.js
@@ -0,0 +1,45 @@
+import React, { useRef, useEffect, useState } from 'react';
+import './App.css';
+import {select} from 'd3';
+
+
+function App() {
+  const [data, setData] =  useState([25,30,45,60,20]);
+
+  const svgRef = useRef();
+  useEffect(() => {
+    const svg = select(svgRef.current);
+    svg
+      .selectAll("circle")
+      .data(data)
+      .join(
+        enter => 
+          enter
+            .append("circle")
+            .attr("class", "my-circle")
+            .attr("r", value => value)
+            .attr("cx", value => value *2)
+            .attr("cy", value => value *2)
+            .attr("stroke", "red"),
+        update => 
+          update.attr("class", "my-circle-updated")
+            .attr("r", value => value)
+            .attr("cx", value => value *2)
+            .attr("cy", value => value *2),
+        exit => exit.remove()
+      );
+  }, [data]);
+
+  return (
+    <>
+      <h3>d3.js start</h3>
+      <div className="content">
+        <svg ref={svgRef}></svg>
+      </div>
+      <button onClick={() => setData(data.map(value => value + 5))}>업데이트</button>
+      <button onClick={() => setData(data.filter(value => value < 35 ))}>필터</button>
+    </>
+  );
+}
+
+export default App;
