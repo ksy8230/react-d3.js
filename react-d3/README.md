491b434574c02eb3d00932e0f048d38febec0749
ksy <ksy8230@hanmail.net>
2020-05-17 09:34:44 +0900

    line 함수를 이용해 x, y 좌표대로 수치가 나타나는 라인선 그리기

--- react-d3/README.md
+++ react-d3/README.md
@@ -6,6 +6,7 @@
 - npm i d3
 - d3 속성값들을 이용해 배열의 값을 radius로 원들 그리기
 - join 값에 공통 적용할 함수들 넣어 코드 줄이기
+- line 함수를 이용해 x, y 좌표대로 수치가 나타나는 라인선 그리기
 
 참고 url
 - https://www.youtube.com/watch?v=9uEmNgHzPhQ&t=641s
--- react-d3/src/App.js
+++ react-d3/src/App.js
@@ -1,16 +1,19 @@
 import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
-import {select} from 'd3';
+import {select, line, curveCardinal} from 'd3';
 
 
 function App() {
   const [data, setData] =  useState([25,30,45,60,20]);
+  const [data2, setData2] =  useState([25, 30, 45, 20, 100, 20, 0]);
 
   const svgRef = useRef();
+  const svgRef2 = useRef();
+
   useEffect(() => {
     const svg = select(svgRef.current);
     svg
-      .selectAll("circle")
+      .selectAll("rect")
       .data(data)
       .join(
         enter => enter.append("circle").attr("class", "my-circle"),
@@ -25,6 +28,21 @@ function App() {
         
   }, [data]);
 
+  useEffect(() => {
+    const svg = select(svgRef2.current);
+    const myLine = line()
+      .x((value, index) => index * 50)
+      .y((value) => 150 - value)
+      .curve(curveCardinal)
+      svg
+        .selectAll("path")
+        .data([data2])
+        .join("path")
+        .attr("d", value => myLine(value))
+        .attr("fill", "none")
+        .attr("stroke", "blue")
+  }, [data2])
+
   return (
     <>
       <h3>d3.js start</h3>
@@ -33,6 +51,11 @@ function App() {
       </div>
       <button onClick={() => setData(data.map(value => value + 5))}>업데이트</button>
       <button onClick={() => setData(data.filter(value => value < 35 ))}>필터</button>
+
+      <h3>d3.js line</h3>
+      <div className="content">
+        <svg ref={svgRef2}></svg>
+      </div>
     </>
   );
 }
